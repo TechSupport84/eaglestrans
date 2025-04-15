9904fa47982a14e8bf2671824f0b578bb5f9f73a
TechSupport84 <trainingtech84@gmail.com>
2025-04-15 17:45:36 +0300

    Added ads

--- src/pages/Home_Page.tsx
+++ src/pages/Home_Page.tsx
@@ -64,8 +64,11 @@ const handledPartner =()=>{
         <button onClick={handledNavigation } className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition duration-300 w-full md:w-auto">
           Réserver un Trajet
         </button>
-        <button  onClick ={ handledPartner }className="px-6 py-3 bg-green-600 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 transition duration-300 w-full md:w-auto">
+        <button  onClick ={ handledPartner }className="px-6 py-3 bg-orange-600 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 transition duration-300 w-full md:w-auto">
           Devenir Partenaire
+        </button>
+        <button  onClick ={ handledPartner }className="px-6 py-3 bg-green-600 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 transition duration-300 w-full md:w-auto">
+          Appel 
         </button>
       </div>
     </div>
