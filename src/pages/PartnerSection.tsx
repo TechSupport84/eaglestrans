e09fc6a1c1472d60625200a2c61729cf2f1c7259
TechSupport84 <trainingtech84@gmail.com>
2025-04-26 20:01:44 +0300

    add

--- src/pages/PartnerSection.tsx
+++ src/pages/PartnerSection.tsx
@@ -24,11 +24,16 @@ const PartnerSection: React.FC = () => {
   const handleCreatePartner = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    // if (user?.id) {
-    //   setError("Ce partenaire existe déjà.");
-    //   toast.error("Ce partenaire existe déjà.");
-    //   return;
-    // }
+    if (!/^(?=(?:.*[a-zA-Z]){2})(?=(?:.*\d){2})[a-zA-Z\d]{4}$/.test(plaqueNumber)) {
+      setError("Le numéro de plaque (PIM) doit contenir exactement 2 lettres et 2 chiffres mélangés (ex: A1B2 ou 1A2B).");
+      toast.error("Le numéro de plaque (PIM) doit contenir exactement 2 lettres et 2 chiffres mélangés (ex: A1B2 ou 1A2B).");
+      return;
+    }
+
+    if (!/^\d{10}$/.test(tel)) {
+    setError("Le numéro de téléphone doit contenir exactement 10 chiffres.");
+    return 
+    }
 
     if (!carName || !plaqueNumber || !tel || !city) {
       setError("Veuillez remplir tous les champs.");
